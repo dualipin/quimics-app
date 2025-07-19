61fd8b7956e5d46fc0a2fc0e4f4e1c30913a1f0e
dualipin <118947989+dualipin@users.noreply.github.com>
2025-07-19 15:54:59 -0600

    feat: hero

--- src/modules/landing/components/home/HeroSection.vue
+++ src/modules/landing/components/home/HeroSection.vue
@@ -4,13 +4,10 @@ import { motion } from 'motion-v'
 </script>
 
 <template>
-  <section class="min-h-dvh py-20 dark:bg-black overflow-hidden">
+  <section class="py-20 min-h-dvh relative grid place-content-center">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="text-center">
         <motion.h1
-          :initial="{ y: -100 }"
-          :animate="{ y: 0 }"
-          :transition="{ duration: 1 }"
           class="text-4xl tracking-tight font-extrabold text-gray-900 dark:text-gray-100 sm:text-5xl md:text-6xl"
         >
           <span class="block">Studying</span>
@@ -30,6 +27,22 @@ import { motion } from 'motion-v'
       <div>
         <MoleculeContainer />
       </div>
+    </div>
+
+    <div class="inset-0 absolute w-full overflow-hidden dark:bg-black bg-white -z-10">
+      <span
+        v-for="key in 20"
+        :key="key"
+        class="blur-2xl absolute aspect-square"
+        :style="{
+          height: `${Math.random() * 200}px`,
+          top: `${Math.random() * 100}%`,
+          left: `${Math.random() * 100}%`,
+          animation: `pulsing 1s ${Math.random() * 5}s ease-in-out both infinite`,
+          borderRadius: `${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% / ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}% ${Math.random() * 100}%`,
+          background: `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)}, 0.8)`,
+        }"
+      ></span>
     </div>
   </section>
 </template>
