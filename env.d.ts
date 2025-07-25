f7ce56a47bae21bd7b2fea89a34028f2bc985821
dualipin <118947989+dualipin@users.noreply.github.com>
2025-07-24 21:47:56 -0600

    feat(env): add websocket configuration to environment variables

--- env.d.ts
+++ env.d.ts
@@ -8,6 +8,8 @@ declare module '*.pdb' {
 interface ImportMetaEnv {
   readonly VITE_API_HOST: string
   readonly VITE_API_URL: string
+  readonly VITE_WEBSOCKET_HOST: string
+  readonly VITE_WEBSOCKET_URL: string
   // You can add other environment variables here as needed
 }
 
