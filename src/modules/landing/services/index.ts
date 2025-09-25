0806bc3155b5f03c3e09341ffebe8dc994c952a7
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2025-09-25 08:30:16 -0600

    prepare to new version

--- src/modules/landing/services/index.ts
+++
@@ -1,5 +0,0 @@
-const BASE_URL = '/v1/landing'
-
-export function getLandingElementAudio(fromPath: string, number: number) {
-  return `${import.meta.env.VITE_API_HOST}/api${BASE_URL}/${fromPath}/${number}/stream`
-}
